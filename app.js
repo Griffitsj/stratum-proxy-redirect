daf9d72204760a49cec48c1754cb98483e8c9615
Steven Griffiths <steve@sgriff.com>
2016-04-20 20:23:44 +0100

    Added files

---
+++ app.js
@@ -0,0 +1,5 @@
+﻿var stratum = require('./stratumRedirect.js');
+
+// Starts a 'stratum proxy' server on localhost:3335 that redirects all traffic to stratum.example.com:3333
+// This might be useful if you are moving your stratum server to another hostname or port and don't want to lose the connections.
+var s = stratum.start('SOMENAME', 3335, 'stratum.example.com', 3333);
---
+++ stratumRedirect.js
@@ -0,0 +1,67 @@
+﻿var net = require('net');
+var util = require('util');
+var events = require("events");
+
+function stratumRedirect(name, listenPort, redirectHost, redirectPort) {
+    events.EventEmitter.call(this);
+    console.log(name + ':init');
+
+    function emitMethod(data) {
+        try {
+            var jsonData = JSON.parse(data);
+            if (jsonData.method) {
+                this.emit(jsonData.method, jsonData);
+            }
+        }
+        finally {
+            this.emit('invalidrequest', data);
+        }
+    }
+
+    net.createServer({ allowHalfOpen: false }, function (socket) {
+        console.log(name + ':new');
+
+        var serviceSocket = new net.Socket();
+        serviceSocket.connect(redirectPort, redirectHost);
+
+        // Write data to the destination host
+        socket.on("data", function (data) {
+            serviceSocket.write(data);
+        });
+
+        // Pass data back from the destination host
+        serviceSocket.on("data", function (data) {
+            console.log(name + ':data');
+            socket.write(data);
+        });
+
+        socket.on("close", function (had_error) {            
+            console.log(name + ':close had_error=' + had_error);
+            serviceSocket.end();
+        })
+
+        serviceSocket.on("close", function (had_error) {
+            socket.end();
+        });
+
+        socket.on("error", function (e) {
+            console.log(name + ':warn', '[' + new Date() + '] Proxy Socket Error');
+            console.log(name + ':warn', e);
+        });
+
+        serviceSocket.on("error", function (e) {
+            console.log(name + ':warn', '[' + new Date() + '] Service Socket Error');
+            console.log(name + ':warn', e);
+        });
+    }).listen(parseInt(listenPort), function () {
+        console.log(name + ':listen listenPort=' + listenPort);
+    });
+}
+
+util.inherits(stratumRedirect, events.EventEmitter);
+
+module.exports = {
+    start: function (name, listenPort, redirectHost, redirectPort) {
+        return new stratumRedirect(name, listenPort, redirectHost, redirectPort);
+    }
+};
